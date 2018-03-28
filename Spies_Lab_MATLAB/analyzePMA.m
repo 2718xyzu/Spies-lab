8740c64c3b9a3d846692fc4caa3764f2aacc1e00
Wayne Bowie <lilwayne1556@gmail.com>
2018-03-28 09:51:38 -0500

    modified:   analyzePMA.m

--- Spies_Lab_MATLAB/analyzePMA.m
+++ Spies_Lab_MATLAB/analyzePMA.m
@@ -2,7 +2,7 @@
 % Last updated 7/26/17
 
 answer = questdlg('One channel or two channels (FRET)?','One channel or two?','Two Channels','One Channel','Two Channels');
- addpath('Functions');
+addpath('Functions');
 if answer(1) == 'O'
     imageRegions1channel();
 else
