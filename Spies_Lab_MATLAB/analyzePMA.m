a13a83479de72d9b857356daf8aebf90762ce58e
Joseph Tibbs <jtibbs@uni.edu>
2018-03-28 10:07:50 -0500

    Test edit from desktop
    I got the desktop editor up and running.  This is a meaningless edit.

--- Spies_Lab_MATLAB/analyzePMA.m
+++ Spies_Lab_MATLAB/analyzePMA.m
@@ -1,8 +1,7 @@
-% Joseph Tibbs
-% Last updated 7/26/17
+addpath('Functions'); %Allow access to 'Functions' folder
 
 answer = questdlg('One channel or two channels (FRET)?','One channel or two?','Two Channels','One Channel','Two Channels');
-addpath('Functions');
+
 if answer(1) == 'O'
     imageRegions1channel();
 else
