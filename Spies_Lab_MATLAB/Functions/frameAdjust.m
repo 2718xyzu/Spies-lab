b7591e75ff0bf74ddeed5e307acd1929b561a3c1
lilwayne1556 <lilwayne1556@gmail.com>
2018-03-28 22:36:37 -0500

    modified:   Spies_Lab_MATLAB/Functions/frameAdjust.m

--- Spies_Lab_MATLAB/Functions/frameAdjust.m
+++ Spies_Lab_MATLAB/Functions/frameAdjust.m
@@ -1,23 +1,15 @@
 function [combinedFrame, BW, adjustedFrame] = frameAdjust(frame,brightness)
-adjustedFrame = zeros(size(frame));
-width = size(frame,2);
-height = size(frame,1);
-for i = 0:7
-    for j=0:7
-        xIndices = 1+i*width/8:(i+1)*width/8;
-        yIndices = 1+j*height/8:(j+1)*height/8;
-%         if mean(xIndices)>256
-%             brightness = brightnessR;
-%         else
-%             brightness = brightnessL;
-%         end
-        [combinedFrameSmall, BWSmall, adjustedFrameSmall, ~] = imageAdjust2(frame(yIndices,xIndices,:),brightness);
-        combinedFrame(yIndices,xIndices) = combinedFrameSmall;
-        BW(yIndices,xIndices) = BWSmall;
-        adjustedFrame(yIndices,xIndices,:) = adjustedFrameSmall;
-
-        
+    adjustedFrame = zeros(size(frame));
+    width = size(frame,2);
+    height = size(frame,1);
+    for i = 0:7
+        for j=0:7
+            xIndices = 1+i*width/8:(i+1)*width/8;
+            yIndices = 1+j*height/8:(j+1)*height/8;
+            [combinedFrameSmall, BWSmall, adjustedFrameSmall, ~] = imageAdjust2(frame(yIndices,xIndices,:),brightness);
+            combinedFrame(yIndices,xIndices) = combinedFrameSmall;
+            BW(yIndices,xIndices) = BWSmall;
+            adjustedFrame(yIndices,xIndices,:) = adjustedFrameSmall;
+        end
     end
-end
-
 end
