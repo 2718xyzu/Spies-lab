e28da6ad1b32c0eb0cb3e0bcfd47898f98e87390
lilwayne1556 <lilwayne1556@gmail.com>
2018-03-28 22:27:04 -0500

    modified:   Spies_Lab_MATLAB/Kera_3.m 	modified:   Spies_Lab_MATLAB/changeToebFRET.m

--- Spies_Lab_MATLAB/changeToebFRET.m
+++ Spies_Lab_MATLAB/changeToebFRET.m
@@ -1,5 +1,5 @@
-clear intensity
 addpath('Functions');
+clear intensity
 
 blank = questdlg('Are your traces in a .traces file or saved individually in txt, csv, or dat?',...
     'Select format','.traces','Individual','Individual');
@@ -7,13 +7,13 @@ blank = questdlg('Are your traces in a .traces file or saved individually in txt
 
 if blank(2) == 't'
     Trace_viewer_myversion();
-    
+
 else
     blank = questdlg('Select the folder which contains all traces to normalize',...
         'Select folder','Ok','Ok');
-    
+
     path = uigetdir;
-    
+
     if ismac
         slash = '/';
     elseif ispc
@@ -21,10 +21,10 @@ else
     else
         slash = '/';
     end
-    
-    
+
+
     format = questdlg('Which format are the traces in?','Select format','.csv','.txt','.dat','.dat');
-    
+
     if format(2) == 'c'
         dir2 = dir([path slash '*.csv']);
         clear dir3;
@@ -37,25 +37,19 @@ else
         dir3 = { dir2.name };
         A = importdata([ path slash dir3{1}]);
         meanA = mean(A);
-        
+
     else
         dir2 = dir([path slash '*.dat']);
         clear dir3;
         dir3 = { dir2.name };
         A = importdata([ path slash dir3{1}]);
         meanA = mean(A);
-        
+
     end
-    
-    
-    % blank = questdlg('Select or make the folder which the program will save traces to',...
-    %     'Select save folder','Ok','Ok');
-    % path2 = uigetdir;
-    % dbstop in change3toEbFRET if naninf
-    
+
     column = inputdlg(['Which column of the data would you like extracted?  If you want each column extracted, type "all".' ...
         ' If it helps, the mean of each column is, in order: ' mat2str(meanA,5) ]);
-    
+
     column = str2double(column{1});
     for q = 1:length(dir3)
         A = importdata([ path slash dir3{q}]);
@@ -64,30 +58,14 @@ else
         end
         intensity(q,1:length(A)) = A(:,column);
     end
-    
 
+    emFRET = emulateFRET(intensity);
+    timeUnit = inputdlg('Input time unit of the video in seconds');
+    if isempty(timeUnit{:})
+        timeUnit = .1;
+    else
+        timeUnit = str2double(timeUnit{1});
+    end
 
-% for q = 1:length(dir3)
-% A = importdata([ path slash dir3{q}]);
-% intensity(q,:) = A(:,column);
-% end
-
-% ones = abs(1-intensity)<(1e-10);
-% randoM = ones.*(randn(size(ones))*.025);
-% intensity = intensity+randoM;
-
-
-emFRET = emulateFRET(intensity);
-
-
-timeUnit = inputdlg('Input time unit of the video in seconds');
-if isempty(timeUnit{:})
-    timeUnit = .1;
-else
-    timeUnit = str2double(timeUnit{1});
+    plotCut3(1-emFRET,emFRET,length(intensity),timeUnit);
 end
-
-plotCut3(1-emFRET,emFRET,length(intensity),timeUnit);
-end
-
-
