6adeed6e2a878a88ba26be66403ddb5e8120b370
lilwayne1556 <lilwayne1556@gmail.com>
2018-03-28 22:38:43 -0500

    modified:   Spies_Lab_MATLAB/Functions/getFitHistogram.m 	modified:   Spies_Lab_MATLAB/Functions/getFret.m 	modified:   Spies_Lab_MATLAB/Functions/getGauss.m

--- Spies_Lab_MATLAB/Functions/getFitHistogram.m
+++ Spies_Lab_MATLAB/Functions/getFitHistogram.m
@@ -1,13 +1,11 @@
 function fitModel = getFitHistogram(h1,fitType,order)
+    x = h1.BinEdges(1:end-1)+diff(h1.BinEdges)/2;
+    y = h1.Values;
+    if fitType == 1
+        model = ['exp' num2str(order)];
+    else
+        model = ['gauss' num2str(order)];
+    end
 
-x = h1.BinEdges(1:end-1)+diff(h1.BinEdges)/2;
-y = h1.Values;
-if fitType == 1
-    model = ['exp' num2str(order)];
-else
-    model = ['gauss' num2str(order)];
-end
-
-fitModel = fit(x',y',model);
-
+    fitModel = fit(x',y',model);
 end
--- Spies_Lab_MATLAB/Functions/getFret.m
+++ Spies_Lab_MATLAB/Functions/getFret.m
@@ -1,5 +1,3 @@
-%Joseph Tibbs
-%Last updated 6/12
 function traceFret = getFret(traceD,traceA)
-traceFret = traceA./(traceD+traceA);
+    traceFret = traceA./(traceD+traceA);
 end
