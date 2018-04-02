4efe31cbd23f3e66de9b148a3533baad864f4313
2718xyzu <30501019+2718xyzu@users.noreply.github.com>
2018-04-02 10:26:50 -0500

    Update parseTransition
    It's not done yet, but it's getting there.

--- Spies_Lab_MATLAB/Functions/parseTransition.m
+++ Spies_Lab_MATLAB/Functions/parseTransition.m
@@ -1,31 +1,40 @@
 function parseTransition(transition, channels, stateList)
-    powerAddition = [0 cumsum(stateList)];
-    matrix = zeros(1+stateList*2);
-    matrixFinal = matrix;
-        for j = 1:channels
-            for i = 1:size(matrix,j)
-                state = i-stateList(j)-1;
-                S.subs = repmat({':'},1,ndims(matrix));
-                S.subs(j) = {i};
-                S.type = '()';
-                if state > stateList(j)
-                    value = 2.^(powerAddition(j)+state-1);
-                elseif state < stateList(j)
-                    value = -1*2.^(powerAddition(j)+state-1);
-                else
-                    value = 0;
-                end
-                matrix = subsasgn(matrix,S,value); %all states represented as binary #'s
-                i=i+1; %move to the next row
-            end
-            matrixFinal = matrixFinal + matrix;
+
+powerAddition = [0 cumsum(stateList)];
+matrix = zeros(1+stateList*2);
+matrixFinal = matrix;
+    for j = 1:channels
+        for i = 1:size(matrix,j)
+            state = i-stateList(j)-1;
+            S.subs = repmat({':'},1,ndims(matrix));
+            S.subs(j) = {i};
+            S.type = '()';
+            value = sign(i-stateList(j)-1)*2.^(powerAddition(j)+abs(state)-1);
+            matrix = subsasgn(matrix,S,value); 
         end
-
-
-    transition(1) = '[';
-    transition(end) = ']';
-    transition = eval(transition);
-    for i=1:length(transition)
-        %Was this meant to be finished?
+        matrixFinal = matrixFinal + matrix;
     end
+
+
+transition(1) = '[';
+transition(end) = ']';
+transition = eval(transition);
+text = ''; 
+
+for i=1:length(transition)
+    subs = cell(1,length(size(matrixFinal)));
+    textAdd = '';
+    index = find(transition(i) == matrixFinal);
+    if ~isempty(index)
+    [subs{:}] = ind2sub(size(matrixFinal),index);
+    if a ~= 0
+        
+    end
+    
+    else
+        textAdd = 'complex';
+    end
+    text = [text '; ' textAdd];
+end
+
 end
