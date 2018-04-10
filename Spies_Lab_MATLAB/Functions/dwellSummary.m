dcb38d0a4f9ffcd78c18481c2e1d09a890a4dd7f
Joseph L Tibbs <jtibbs@uni.edu>
2018-04-10 14:36:27 -0500

    Added comments
    Added comments to this function, so it hopefully makes more sense.

--- Spies_Lab_MATLAB/Functions/dwellSummary.m
+++ Spies_Lab_MATLAB/Functions/dwellSummary.m
@@ -1,16 +1,18 @@
 function out = dwellSummary(matrix,timeInterval,channels)
-    for k = 1:channels
-        out(k).timeBeforeFirst(1) = 0;
+    for k = 1:channels %initialize the fields; each row corresponds to a channel
+        out(k).timeBeforeFirst(1) = 0; %the time at ground before the first event
         out(k).timeBeforeFirst(1) = [];
-        out(k).timeAfterLast(1) = 0;
+        out(k).timeAfterLast(1) = 0;%the time spent at ground after last event
         out(k).timeAfterLast(1) = [];
-        out(k).dwellTimes(1,:) = 1:max(matrix(:));
+        out(k).dwellTimes(1,:) = zeros([1,max(matrix(:))]); 
+        %an exhaustive list of all times spent at a given state, where each
+        %column corresponds to a different state (column 1 to state 1 etc.)
     end
 
     for j = 1:size(matrix,2)
-        tempList = matrix(1:nnz(matrix(:,j)),j);
-        if sum(tempList) ~= length(tempList)
-            k = mod(j-1,channels)+1;
+        tempList = matrix(1:nnz(matrix(:,j)),j); %extract column, truncate trailing zeros
+        if sum(tempList) ~= length(tempList) %if the trajectory is not all ones
+            k = mod(j-1,channels)+1; %assign this data to the proper channel
             i = 1;
             state = tempList(i);
 
@@ -20,7 +22,7 @@ function out = dwellSummary(matrix,timeInterval,channels)
             end
 
             if i>1 && i~=tempList(i)
-                out(k).timeBeforeFirst(end+1) = i-1;
+                out(k).timeBeforeFirst(end+1) = i-1; %time spent at ground before first event
             end
 
             iLast = i-1;
@@ -28,7 +30,7 @@ function out = dwellSummary(matrix,timeInterval,channels)
                 state = tempList(i);
                 stateNext = tempList(i+1);
                 if state~=stateNext
-                    longth = i - iLast;
+                    longth = i - iLast; %time spent at this state; append to list in appropriate col
                     out(k).dwellTimes(nnz(out(k).dwellTimes(:,state))+1,state) = longth;
                     iLast = i;
                 end
@@ -38,13 +40,13 @@ function out = dwellSummary(matrix,timeInterval,channels)
 
             if tempList(end) == 1
                 longth = i - iLast;
-                out(k).timeAfterLast(end+1) = longth;
+                out(k).timeAfterLast(end+1) = longth; %time after last event spent at ground (if any)
             end
         end
     end
-    for k = 1:channels
+    for k = 1:channels %scale by factor of time interval (converts to seconds)
         out(k).timeBeforeFirst = (out(k).timeBeforeFirst').*timeInterval;
         out(k).timeAfterLast = (out(k).timeAfterLast').*timeInterval;
-        out(k).dwellTimes(2:end,:) = out(k).dwellTimes(2:end,:).*timeInterval;
+        out(k).dwellTimes = out(k).dwellTimes.*timeInterval;
     end
 end
