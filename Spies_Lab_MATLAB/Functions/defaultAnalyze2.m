35e47cb0da5c46b82e5baa4285214dd44a30fe91
Joseph L Tibbs <jtibbs@uni.edu>
2018-04-11 09:04:29 -0500

    Switched 3 files from Master

--- Spies_Lab_MATLAB/Functions/defaultAnalyze2.m
+++ Spies_Lab_MATLAB/Functions/defaultAnalyze2.m
@@ -1,40 +1,44 @@
 function output = defaultAnalyze2(results)
     out = regExAnalyzer2('_[^_,]{3,}_',results);
+    %the above line searches the transition matrix (nonZeros) for all
+    %'completed' events
     C = out.eventList;
     nums = cellfun(@(x) mat2str(x),C,'UniformOutput',false);  
     u = unique(nums);
+    %find all unique classifications of a 'completed' event
 
-    for i = 2:numel(u)+1
-        expr1 = eval(u{i-1});
-        expr2 = mat2str(expr1);
-        expr2 = regexprep(expr2,'[ ;]','  ');
-        expr2 = expr2(2:end-1);
-        output(i).expr = {['_  ' expr2 '  _']};
+    for i = 2:numel(u)+1 %turn each event found into the text-search form
+        expr2 = regexprep(u{i-1},'[ ;]','  ');
+        expr2 = expr2(2:end-1); %remove the brackets from the string
+        output(i).expr = {['_  ' expr2 '  _']}; %underscores signal the ground state
     end
 
-    output(1).expr = {'_[^_,]{3,}_'};
+    output(1).expr = {'_[^_,]{3,}_'}; %again, the expression which searches for all 
+    %completed events
     rows = size(output,2);
 
     for i = 1:rows
-        expr = output(i).expr{:};
-        out = regExAnalyzer2(expr,results);
+        expr = output(i).expr{:}; %the text to search for
+        out = regExAnalyzer2(expr,results); %function which does the searching
         output(i).count = out.numEvents;
         output(i).meanLength = mean(out.timeLengths);
         output(i).eventList = out.eventList;
-        output(i).timeLengths = out.timeLengths;
+        output(i).timeLengths = out.timeLengths; %how long was each event
 
-        if ~isfield(out,'names')
+        if ~isfield(out,'names') %if name data exists
             out.names = out.timeLengths.*0;
         end
 
-        if out.numEvents>0
+        if out.numEvents>0 %create a detailed table and store it in its own field
         output(i).table = table(output(i).eventList,output(i).timeLengths,...
             out.timeList,out.timeDiff,out.begin,out.last,out.names,'VariableNames',...
             {'Events','Total_Duration','Time_Points','Delta_t','Time_first','Time_last','File'});
         end
-
+        %the next line uses lookaround to search for the all 'gaps' which
+        %are periods of ground state immediately preceded and followed by
+        %the type of event which we are currently looking at.
         expr2 = ['(?<=(' output(i).expr{:}(2:end-1) '))_  _(?=' output(i).expr{:}(2:end-1) ')'];
-        out = regExAnalyzer2(expr2,results);
+        out = regExAnalyzer2(expr2,results); %again, get information about any gaps
         output(i).count_Gaps = out.numEvents;
         output(i).meanLength_Gaps = mean(out.timeLengths);
         output(i).timeLengths_Gaps = out.timeLengths;
