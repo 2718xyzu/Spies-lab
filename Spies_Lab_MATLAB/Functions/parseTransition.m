ee1ed39372b29a9fb1ab92a46d7f9bc67c337956
Joseph <jtibbs@uni.edu>
2019-08-31 17:50:35 -0500

    Visualization and transition parsing written
    Not yet integrated into GUI, but can now take in arbitrary string of transitions and determine the states transitioned between to display a graphical representation

--- Spies_Lab_MATLAB/Functions/parseTransition.m
+++ Spies_Lab_MATLAB/Functions/parseTransition.m
@@ -1,40 +1,50 @@
-function parseTransition(transition, channels, stateList)
+function [stateRecord, text] = parseTransition(transition, channels, stateList)
 
-powerAddition = [0 cumsum(stateList)];
-matrix = zeros(1+stateList*2);
+powerAddition = repmat([0 cumsum(stateList(1:end-1))],[1,2]);
+matrix = zeros([stateList stateList]);
 matrixFinal = matrix;
-    for j = 1:channels
+    for j = 1:channels*2
         for i = 1:size(matrix,j)
-            state = i-stateList(j)-1;
+            state = i;
             S.subs = repmat({':'},1,ndims(matrix));
             S.subs(j) = {i};
             S.type = '()';
-            value = sign(i-stateList(j)-1)*2.^(powerAddition(j)+abs(state)-1);
+            value = ((j>channels)*2-1)*2.^(powerAddition(j)+state-1);
             matrix = subsasgn(matrix,S,value); 
         end
         matrixFinal = matrixFinal + matrix;
     end
 
-
-transition(1) = '[';
-transition(end) = ']';
-transition = eval(transition);
+if transition(1) == '_' && transition(end) == '_'
+    transition(1) = '[';
+    transition(end) = ']';
+else
+    transition = ['[' transition ']'];
+end
+transition = eval(transition); %turning the text back into an array
 text = ''; 
-
+initialText = '';
+stateRecord = zeros([channels,length(transition)+1]);
 for i=1:length(transition)
     subs = cell(1,length(size(matrixFinal)));
     textAdd = '';
-    index = find(transition(i) == matrixFinal);
+    index = find(transition(i) == matrixFinal,1);
     if ~isempty(index)
-    [subs{:}] = ind2sub(size(matrixFinal),index);
-    if a ~= 0
-        
-    end
+        [subs{:}] = ind2sub(size(matrixFinal),index);
+        for k = find(diff(cell2mat([subs(1:channels); subs(channels+1:end)])))
+            if stateRecord(k,1) == 0 
+                stateRecord(k,1:i) = subs{k};
+                initialText = [initialText 'Channel ' num2str(k) ' starts in state ' num2str(subs{k}) '; '];
+            end
+            stateRecord(k,i+1:end) = subs{channels+k};
+            textAdd = ['Channel ' num2str(k) ' transitions to state ' num2str(subs{channels+k})];
+        end
     
     else
-        textAdd = 'complex';
+        textAdd = 'invalid transition number';
     end
     text = [text '; ' textAdd];
 end
-
+text = [initialText, text];
+stateRecord = stateRecord';
 end
---
+++ Spies_Lab_MATLAB/Functions/visualizeTransition.m
@@ -0,0 +1,10 @@
+function [x, y, text] = visualizeTransition(transition, channels, stateList)
+    [stateRecord, text] = parseTransition(transition, channels, stateList);
+    y = reshape(repmat(reshape(stateRecord,[1 numel(stateRecord)]),[2 1]),[2*size(stateRecord,1) channels]);
+    x = 1; 
+    for i = 1:size(y,1)/2-1
+        x(2*i:2*i+1) = i+1;
+    end
+    x = [x x(end)+1];
+    x = repmat(x',[1 channels]);
+end
