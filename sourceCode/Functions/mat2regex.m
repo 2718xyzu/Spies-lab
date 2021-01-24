261851aefc6e78032a620013f0394b3f98964682
Joseph Tibbs <jtibbs@uni.edu>
2021-01-24 14:25:24 -0600

    Implemented automatic lookahead/lookbehind in regex translator
    This just allows the regex search to have the same behavior that the old search (findStateEvents) while running faster than the for loop-based method.

--- sourceCode/Functions/mat2regex.m
+++ sourceCode/Functions/mat2regex.m
@@ -1,10 +1,19 @@
 function outChar = mat2regex(stateMat)
+N = size(stateMat,1); %number of lines to analyze
 c = size(stateMat,2); %number of channels
 outStr = "";
-for i = 1:size(stateMat,1)
+for i = 1:N
     if any(isinf(stateMat(i,:)))
-        outStr = outStr+"[^\]]+?";
+        if i~=1 && i ~=N %including this wildcard at the beginning or end of a search doesn't make sense
+            outStr = outStr+"[^\]]+?";
+        end
     else
+        if i == 1 && N>2
+            outStr = outStr+"(?<="; %make the first state a positive lookbehind
+        end
+        if i == N && N>2
+            outStr = outStr+"(?="; %make the last state a positive lookahead
+        end
         for j = 1:size(stateMat,2)
             if ~isnan(stateMat(i,j))
                 outStr = outStr+" "+string(num2str(stateMat(i,j)))+" ";
@@ -13,6 +22,9 @@ for i = 1:size(stateMat,1)
             end
         end
         outStr = outStr+";";
+        if i == 1 || i == N && N>2
+            outStr = outStr+")";
+        end
     end
 end
 outChar = char(outStr);
